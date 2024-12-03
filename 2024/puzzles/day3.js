e7496f099cecf0af622134c424ebb9c8dbabde19
David Ku <dku7@proton.me>
2024-12-03 12:04:02 +0000

    destructure variables and use higher-order function

--- 2024/puzzles/day3.js
+++ 2024/puzzles/day3.js
@@ -9,29 +9,34 @@ const getRegEx = (enabledOnly) => {
 };
 
 const multiply = (expression) => {
-  const digits = expression.split(",");
-  return Number(digits[0]) * Number(digits[1]);
+  const [digit1, digit2] = expression.split(",");
+  return Number(digit1) * Number(digit2);
+};
+
+// take in total, and enabled in COVE
+// return fun that accepts command and expression
+
+const getProcessFunction = (calculationsEnabled) => (command, expression) => {
+  switch (command) {
+    case "don't()":
+      calculationsEnabled = false;
+      return 0;
+    case "do()":
+      calculationsEnabled = true;
+      return 0;
+    default:
+      return calculationsEnabled ? multiply(expression) : 0;
+  }
 };
 
 const sumInstructions = (input, enabledOnly = false) => {
   const instructions = input.matchAll(getRegEx(enabledOnly));
   let total = 0;
-  let calculationsEnabled = true;
+  const processInstruction = getProcessFunction(true);
 
   for (const instruction of instructions) {
-    const command = instruction[0];
-    const expression = instruction[1];
-
-    switch (command) {
-      case "don't()":
-        calculationsEnabled = false;
-        break;
-      case "do()":
-        calculationsEnabled = true;
-        break;
-      default:
-        total += calculationsEnabled ? multiply(expression) : 0;
-    }
+    const [command, expression] = instruction.slice(0, 2);
+    total += processInstruction(command, expression);
   }
 
   return total;
