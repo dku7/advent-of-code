396232ab18cba31f6769ab9d6b16c90a83fabaaa
David Ku <dku7@proton.me>
2024-12-06 21:27:37 +0000

    refactor day 6

--- 2024/src/day6.js
+++ 2024/src/day6.js
@@ -9,30 +9,20 @@ const FACING_RIGHT = "RIGHT";
 const FACING_LEFT = "LEFT";
 const directions = [FACING_UP, FACING_RIGHT, FACING_DOWN, FACING_LEFT];
 
-const markerDirections = {
-  "^": FACING_UP,
-  v: FACING_DOWN,
-  ">": FACING_RIGHT,
-  "<": FACING_LEFT,
-};
-
 const getMarkerPos = (line) => line.search(/(\^|v|<|>)/);
+const obstacleFound = (marker) => marker === "#";
+const move = (map, x, y) =>
+  obstacleFound(map[y][x])
+    ? { successful: false, coords: [] }
+    : { successful: true, coords: [x, y] };
 
-const getStartCoords = (map) => {
-  const coords = { x: -1, y: -1 };
-
+function getStartCoords(map) {
   for (let y = 0; y < map.length; y++) {
     const markerPos = getMarkerPos(map[y]);
 
-    if (markerPos !== -1) {
-      coords.x = markerPos;
-      coords.y = y;
-      break;
-    }
+    if (markerPos !== -1) return { x: markerPos, y };
   }
-
-  return coords;
-};
+}
 
 function getFacingDirection(line) {
   if (line.indexOf(MARKER_UP) !== -1) return FACING_UP;
@@ -41,20 +31,29 @@ function getFacingDirection(line) {
   if (line.indexOf(MARKER_LEFT) !== -1) return FACING_LEFT;
 }
 
-const obstacleFound = (marker) => marker === "#";
+const go = (map, startCoords, direction) => {
+  switch (direction) {
+    case FACING_UP:
+      return goUp(map, startCoords);
+    case FACING_DOWN:
+      return goDown(map, startCoords);
+    case FACING_RIGHT:
+      return goRight(map, startCoords);
+    case FACING_LEFT:
+      return goLeft(map, startCoords);
+  }
+};
 
-const move = (map, x, y) => (obstacleFound(map[y][x]) ? [] : [x, y]);
-
-const travelDown = (map, startCoords) => {
+const goDown = (map, startCoords) => {
   let moves = [];
   const x = startCoords.x;
   const newCoords = { ...startCoords };
   let obstacle = false;
 
   for (let y = startCoords.y + 1; y < map.length; y++) {
-    const step = move(map, x, y);
-    if (step.length > 0) {
-      moves.push([...step]);
+    const moved = move(map, x, y);
+    if (moved.successful) {
+      moves.push([...moved.coords]);
       newCoords.y++;
     } else {
       obstacle = true;
@@ -65,16 +64,16 @@ const travelDown = (map, startCoords) => {
   return { moves, newCoords, obstacle };
 };
 
-const travelUp = (map, startCoords) => {
+const goUp = (map, startCoords) => {
   let moves = [];
   const x = startCoords.x;
   const newCoords = { ...startCoords };
   let obstacle = false;
 
   for (let y = startCoords.y - 1; y >= 0; y--) {
-    const step = move(map, x, y);
-    if (step.length > 0) {
-      moves.push([...step]);
+    const moved = move(map, x, y);
+    if (moved.successful) {
+      moves.push([...moved.coords]);
       newCoords.y--;
     } else {
       obstacle = true;
@@ -85,16 +84,16 @@ const travelUp = (map, startCoords) => {
   return { moves, newCoords, obstacle };
 };
 
-const travelRight = (map, startCoords) => {
+const goRight = (map, startCoords) => {
   let moves = [];
   const y = startCoords.y;
   const newCoords = { ...startCoords };
   let obstacle = false;
 
   for (let x = startCoords.x + 1; x <= map[y].length; x++) {
-    const step = move(map, x, y);
-    if (step.length > 0) {
-      moves.push([...step]);
+    const moved = move(map, x, y);
+    if (moved.successful) {
+      moves.push([...moved.coords]);
       newCoords.x++;
     } else {
       obstacle = true;
@@ -105,16 +104,16 @@ const travelRight = (map, startCoords) => {
   return { moves, newCoords, obstacle };
 };
 
-const travelLeft = (map, startCoords) => {
+const goLeft = (map, startCoords) => {
   let moves = [];
   const y = startCoords.y;
   const newCoords = { ...startCoords };
   let obstacle = false;
 
   for (let x = startCoords.x - 1; x >= 0; x--) {
-    const step = move(map, x, y);
-    if (step.length > 0) {
-      moves.push([...step]);
+    const moved = move(map, x, y);
+    if (moved.successful) {
+      moves.push([...moved.coords]);
       newCoords.x--;
     } else {
       obstacle = true;
@@ -132,61 +131,22 @@ const turn = (direction) => {
   return newDirection;
 };
 
-const getNextCoord = (direction, coords) => {
-  const newCoords = { ...coords };
-  switch (direction) {
-    case FACING_UP:
-      newCoords.y--;
-      break;
-    case FACING_DOWN:
-      newCoords.y++;
-      break;
-    case FACING_RIGHT:
-      newCoords.x++;
-      break;
-    case FACING_LEFT:
-      newCoords.x--;
-      break;
-  }
-
-  return newCoords;
-};
-
-const alreadyTravelled = (plots, coords) =>
-  plots.find((plot) => plot[0] === coords.x && plot[1] === coords.y);
-
 function travel(map, startCoords, direction) {
   let totalMoves = [];
   let travelled = {};
-  let newDirection = direction;
+  let towards = direction;
   let coords = startCoords;
 
   while (true) {
-    switch (newDirection) {
-      case FACING_UP:
-        travelled = travelUp(map, coords);
-        break;
-      case FACING_DOWN:
-        travelled = travelDown(map, coords);
-        break;
-      case FACING_RIGHT:
-        travelled = travelRight(map, coords);
-        break;
-      case FACING_LEFT:
-        travelled = travelLeft(map, coords);
-        break;
-      default:
-        return totalMoves;
-    }
-
+    travelled = go(map, coords, towards);
     totalMoves = [...totalMoves, ...travelled.moves];
 
     if (travelled.moves.length > 0) {
       if (travelled.obstacle) {
-        newDirection = turn(newDirection);
+        towards = turn(towards);
         coords = travelled.newCoords;
       } else break;
-    } else break; // we didn't go anywhere
+    } else break;
   }
 
   return totalMoves;
